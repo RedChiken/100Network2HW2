2488e9022b37a3268ca8ca1caed521a0974d86f8
RedChiken <gombabo1217@gmail.com>
2018-03-16 23:51:16 +0900

    TCP semi done
    에러 처리 등이 필요. udp도 해야 함

--- Python/SimpleEchoTCPClient.py
+++ Python/SimpleEchoTCPClient.py
@@ -5,18 +5,54 @@
 from socket import *
 
 serverName = 'nsl2.cau.ac.kr'
+localhost = 'localhost'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+clientSocket.connect((localhost, serverPort))
 
 print("The client is running on port", clientSocket.getsockname()[1])
 
-message = input('Input lowercase sentence: ')
+print("Press number of option to select it\n"
+      "option 1) convert text to UPPER-case letters\n"
+      "option 2) convert text to LOWER-case letters\n"
+      "option 3) ask the server what is the IP address and port number of the client\n"
+      "option 4) ask the server what the current time on the server is\n"
+      "option 5) exit client program\n")
 
-clientSocket.send(message.encode())
+option = input("option : ")
+clientSocket.send(option.encode())
+if option == '1':
+    #lowercase to uppercase
+    message = input('Input lowercase sentence: ')
+    clientSocket.send(message.encode())
+    modifiedMessage = clientSocket.recv(2048)
+    print('Reply from server:', modifiedMessage.decode())
 
-modifiedMessage = clientSocket.recv(2048)
-print('Reply from server:', modifiedMessage.decode())
+elif option == '2':
+    #uppercase to lowercase
+    message = input('Input uppercase sentence: ')
+    clientSocket.send(message.encode())
+    modifiedMessage = clientSocket.recv(2048)
+    print('Reply from server:', modifiedMessage.decode())
 
+elif option == '3':
+    #get server's ip and port number
+    modifiedMessage = clientSocket.recv(2048)
+    print('Reply from server:', modifiedMessage.decode())
+    modifiedMessage = clientSocket.recv(2048)
+    print('Reply from server:', modifiedMessage.decode())
+
+elif option  == '4':
+    #get server's time
+    modifiedMessage = clientSocket.recv(2048)
+    print('Reply from server:', modifiedMessage.decode())
+
+elif option == '5':
+    #exit
+    exit()
+
+else:
+    #error case
+    print("")
 clientSocket.close()
--- Python/SimpleEchoTCPServer.py
+++ Python/SimpleEchoTCPServer.py
@@ -2,10 +2,11 @@
 # SimpleEchoTCPServer.py
 #
 
-from socket import *
+import socket
+from datetime import datetime
 
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
@@ -14,8 +15,21 @@ print("The server is ready to receive on port", serverPort)
 while True:
     (connectionSocket, clientAddress) = serverSocket.accept()
     print('Connection requested from', clientAddress)
-    message = connectionSocket.recv(2048)
-    modifiedMessage = message.decode().upper()
-    connectionSocket.send(modifiedMessage.encode())
+    option = connectionSocket.recv(2048).decode()
+    if option == "1":
+        message = connectionSocket.recv(2048)
+        modifiedMessage = message.decode().upper()
+        connectionSocket.send(modifiedMessage.encode())
+    elif option == "2":
+        message = connectionSocket.recv(2048)
+        modifiedMessage = message.decode().lower()
+        connectionSocket.send(modifiedMessage.encode())
+    elif option == "3":
+        connectionSocket.send(str(socket.gethostbyname(socket.gethostname())).encode())
+        connectionSocket.send(str(serverPort).encode())
+    elif option == "4":
+        connectionSocket.send(str(datetime.now()).encode())
+    else:
+        print("error")
     connectionSocket.close()
 
