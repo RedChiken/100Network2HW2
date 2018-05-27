e5423f4d8e702f78042ee1172c91aa4a3dd4e095
RedChiken <gombabo1217@gmail.com>
2018-05-28 00:02:16 +0900

    find solution
    rececive 할 때 에러처리를 해줘야 한다.
    새로운 문제는 input이 한번 씹힌다는 것이다

--- Python/udptest.py
+++ Python/udptest.py
@@ -2,36 +2,43 @@
 
 import socket
 import threading
+import time
+import sys
 from datetime import datetime
 
-serverPort = 26561
+serverPort = 36561
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 # serverSocket.settimeout(5)
 
 
 def send():
+    serverSocket.sendto(str("hi ukio").encode(), ('localhost', 26561))
     while True:
         message = input("input: ")
-        serverSocket.sendto(message.encode(), ('localhost', 36561))
+        serverSocket.sendto(message.encode(), ('localhost', 26561))
 
 
 def receive():
+    message = b''
+    clientAddress = None
     while True:
         try:
             message, clientAddress = serverSocket.recvfrom(2048)
-        except socket.timeout:
+        except ConnectionResetError:
+            print("ukio is not here")
             pass
         print('Connection requested from', clientAddress)
         print(message.decode())
+        # TODO : input 한번 씹힘.
+
 
 
 print("The server is ready to receive on port", serverPort)
-serverSocket.sendto(str("hi ukio").encode(), ('localhost', 36561))
 sthread = threading.Thread(target=send)
 rthread = threading.Thread(target=receive)
-sthread.start()
 rthread.start()
+sthread.start()
 sthread.join()
 rthread.join()
 
