23f46b9fb042a9337e7fbb6938c34581286858af
RedChiken <gombabo1217@gmail.com>
2018-05-29 19:11:24 +0900

    test while loop

--- Python/udptest.py
+++ Python/udptest.py
@@ -14,27 +14,24 @@ class P2P(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((' ', peer_list[node_id]))
         # self.sock.setblocking(0)
-        # self.sock.settimeout(0)
-        self.receive_thread = None
+        # self.sock.settimeout(0.5)
+        # self.receive_thread = None
         self.node_id = id
 
     def send(self):
-        self.receive_thread = threading.Thread(args=self.receive)
-        self.receive_thread.start()
-        self.receive_thread.join()
+        # self.receive_thread = threading.Thread(args=self.receive)
+        # self.receive_thread.start()
+        # self.receive_thread.join()
         while True:
-            message = input()
+            message = input("input: ")
             self.sock.sendto(message.encode(), ('localhost', peer_list[1]))
-
-    def receive(self):
-        while True:
             try:
                 message, clientAddress = self.sock.recvfrom(16000)
-                print(message.decode())
             except ConnectionResetError:
                 print("ConnectionResetError")
             except BlockingIOError:
                 print("BlockingIOError")
+            print(message.decode())
 
 
 if __name__ == "__main__":
